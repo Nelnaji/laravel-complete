598f7497ea9dd21ef6e01110295a1e767b65f0bb
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-09-26 16:11:28 +0200

    closing doing mailing

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,58 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Barryvdh\Debugbar\Facades\Debugbar;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+GET = Request a resource
+POST = Create a new resource
+PUT = Update a resource (every single value in a single low)
+PATCH = Modify a resource (only the value that has been modified)
+DELETE = Delete a resource
+OPTIONS = Ask the server which vers are allowed
 */
 
-Route::resource('blog', PostController::class);
+// GET
+Route::get('blog', [PostController::class,'index']);
+Route::get('blog/{id}', [PostController::class,'show']);
+
+// POST
+Route::get('blog/create', [PostController::class,'create']);
+Route::post('blog', [PostController::class,'store']);
+
+// PUT OR PATCH
+Route::get('blog/edit/{id}', [PostController::class,'edit']);
+Route::patch('blog/{id}', [PostController::class,'update']);
+
+// DELETE
+Route::delete('blog/{id}', [PostController::class,'destroy']);
+
+
+
+
+
+// Route::resource('blog', PostController::class);
+
+Route::get('/', HomeController::class);
+
+
+
+
+
+
+// Multiple HTTP verbs
+// Route::match(['GET','POST'], [PostController::class, 'index']);
+
+// Route::any('blog', [PostController::class, 'index']);
+
+// Return a view
+
+// Route::view('blog', 'blog.index', ['name' => 'habibi']);
+
+
+
 
 // Route for the invoke method
 
